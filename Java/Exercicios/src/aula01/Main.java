c934df209361495462615710545936777cb84f07
Kleber Jardim <silva.dkleber@gmail.com>
2026-07-25 11:05:16 -0300

    Primeiro exercícios feito: Ola kleber

--- Java/Exercicios/src/aula01/Main.java
+++ Java/Exercicios/src/aula01/Main.java
@@ -1,5 +1,17 @@
 package aula01;
+/*1. Impressora Ola e Nome
+
+Escreva um programa em Java para imprimir 'Ola' na tela e seu nome em uma linha separada.
+Saida esperada :
+Ola
+Kleber*/
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        System.out.println("Ola\nKleber");
+
+    }
+
 }
