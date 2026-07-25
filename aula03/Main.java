d1cba06fe945f699fda8d1e6736c1b9c443dd095
kleberJDSilva <silva.dkleber@gmail.com>
2026-07-25 15:54:24 +0000

    inclusão terceira aula

---
+++ aula03/Main.java
@@ -0,0 +1,6 @@
+/*Escreva um programa em Java que receba dois números como entrada e exiba o produto deles.
+Dados de teste:
+Primeiro número: 25
+Segundo número: 5
+Saída esperada :
+25 x 5 = 125 */
