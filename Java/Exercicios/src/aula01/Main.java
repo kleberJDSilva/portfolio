6b6d64652596047033924a375b7dda5ac1c3c866
Kleber Jardim <silva.dkleber@gmail.com>
2026-07-25 12:25:00 -0300

    resolução do Exercício 02 - Soma de dois números

--- Java/Exercicios/src/aula01/Main.java
+++ Java/Exercicios/src/aula01/Main.java
@@ -7,7 +7,6 @@ Ola
 Kleber*/
 
 public class Main {
-
     public static void main(String[] args) {
 
         System.out.println("Ola\nKleber");
---
+++ Main.java
@@ -0,0 +1,11 @@
+package aula02;
+
+public class Main {
+    public static void main(String[] args){
+        int a = 24;
+        int b = 26;
+        int soma = a + b;
+        System.out.println("A soma de a + b é: " + soma);
+    }
+    
+}
